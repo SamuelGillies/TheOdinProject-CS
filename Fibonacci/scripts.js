00926533bdcbb38d1fc5da3ed2ebef02f5c6ff1a
Sam Gillies <sam@samgillies.com>
2023-04-18 17:34:48 +0100

    mergesort function complete

--- Fibonacci/scripts.js
+++ Fibonacci/scripts.js
@@ -31,3 +31,45 @@ function fibsRec(n, array = [0, 1]) {
 }
 
 console.log(fibsRec(8)); 
+
+//Build a function mergeSort that takes in an array and returns a sorted array, using a recursive merge sort methodology.
+
+function mergeSort(array) {
+    if (array.length === 0) return 'not a valid array'; 
+    if (array.length === 1) return array; 
+    
+    let midPoint = Math.floor(array.length / 2); 
+    let left = array.slice(0, midPoint); 
+    let right = array.slice(midPoint); 
+
+    left = mergeSort(left); 
+    right = mergeSort(right); 
+
+    return merge(left, right); 
+    
+}
+
+function merge(left, right) {
+    let merged = []; 
+    let i = 0; 
+    let j = 0; 
+
+    while ((i < left.length) && (j < right.length)) {
+        if (left[i] < right[j]){ 
+            merged.push(left[i]);
+            i++; 
+        } else {
+            merged.push(right[j]); 
+            j++; 
+        }
+    }
+    console.log(left.slice(i));
+    merged = merged.concat(left.slice(i)).concat(right.slice(j));
+
+    return merged;
+}
+
+console.log(mergeSort([0, 2, 4, 3, 2, 3, 4, 5]));
+console.log(mergeSort([0]));
+console.log(mergeSort([-3]));
+console.log(mergeSort([]));
